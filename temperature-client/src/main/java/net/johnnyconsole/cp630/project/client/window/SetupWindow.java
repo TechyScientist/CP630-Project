c1a3aa278a9e53dddf5795aa72b95f29d46cb51f
Johnny Console <johnny.console@hotmail.com>
2023-11-02 09:44:36 -0400

    Add functionality to Setup Window Generate Database Button

--- temperature-client/src/main/java/net/johnnyconsole/cp630/project/client/window/SetupWindow.java
+++ temperature-client/src/main/java/net/johnnyconsole/cp630/project/client/window/SetupWindow.java
@@ -1,5 +1,7 @@
 package net.johnnyconsole.cp630.project.client.window;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import com.mysql.cj.jdbc.Driver;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
@@ -9,6 +11,10 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import net.johnnyconsole.cp630.project.client.TemperatureClient;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 
 public class SetupWindow extends Application {
 
@@ -32,12 +38,24 @@ public class SetupWindow extends Application {
         back.setMaxWidth(Double.MAX_VALUE);
         back.setMinHeight(40);
 
+        model.setDisable(true);
+        genRegression.setDisable(true);
+
         pane.add(createDB,0,0,2,1);
 
         pane.addRow(2, new Label("Regression Model Name:"), model);
         pane.add(genRegression, 0, 3, 2, 1);
 
         pane.add(back, 0, 5, 2, 1);
+
+        createDB.setOnAction(e -> {
+            if(createDatabase()) {
+                createDB.setDisable(true);
+                createDB.setText("Database Created");
+                model.setDisable(false);
+                genRegression.setDisable(false);
+            }
+        });
 
 
         back.setOnAction(e -> {
@@ -53,5 +71,43 @@ public class SetupWindow extends Application {
         ps.setScene(new Scene(pane));
         ps.setTitle("Setup");
         ps.show();
+    }
+
+    private boolean createDatabase() {
+        try {
+            String url = "jdbc:mysql://localhost:3306/test?serverTimezone=America/Toronto",
+                    username = "root",
+                    password = "",
+                    adminPassword = BCrypt.withDefaults().hashToString(12, "Admin123!".toCharArray());
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection(url, username, password);
+
+            String sql = "CREATE TABLE IF NOT EXISTS cons3250_project_model (" +
+                    "name VARCHAR(100) PRIMARY KEY NOT NULL," +
+                    "class TINYTEXT NOT NULL," +
+                    "object BLOB NOT NULL" +
+                    ");";
+
+            Statement stmt = conn.createStatement();
+            stmt.execute(sql);
+
+            sql = "CREATE TABLE IF NOT EXISTS cons3250_project_user (" +
+                    "username VARCHAR(100) PRIMARY KEY NOT NULL," +
+                    "name TINYTEXT NOT NULL," +
+                    "password LONGTEXT NOT NULL," +
+                    "accessLevel INT NOT NULL DEFAULT 0" +
+                    ");";
+            stmt.execute(sql);
+
+            sql = "INSERT IGNORE INTO cons3250_project_user (username, name, password) VALUES('admin', 'Administrator','" + adminPassword + "');";
+            stmt.execute(sql);
+
+            return true;
+        } catch(Exception e ) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.err.flush();
+            return false;
+        }
     }
 }
