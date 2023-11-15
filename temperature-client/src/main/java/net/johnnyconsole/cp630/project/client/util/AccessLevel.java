477055f5cf293d995ac48a16fb81bad27f101efc
Johnny Console <johnny.console@hotmail.com>
2023-11-15 15:04:11 -0500

    Add Util Classes AccessLevel and ApplicationSession

---
+++ temperature-client/src/main/java/net/johnnyconsole/cp630/project/client/util/AccessLevel.java
@@ -0,0 +1,7 @@
+package net.johnnyconsole.cp630.project.client.util;
+
+public class AccessLevel {
+
+    public static final int ACCESS_STANDARD = 0,
+                            ACCESS_ELEVATED = 1;
+}
---
+++ temperature-client/src/main/java/net/johnnyconsole/cp630/project/client/util/ApplicationSession.java
@@ -0,0 +1,9 @@
+package net.johnnyconsole.cp630.project.client.util;
+
+public class ApplicationSession {
+
+    public static String username, name;
+    public static int accessLevel;
+
+
+}
