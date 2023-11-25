3ea5a13c5bdc7ab50f2cb2f877d252bfe7a55c57
Johnny Console <johnny.console@hotmail.com>
2023-11-25 09:00:48 -0500

    Enable Predictions in Client

--- temperature-client/src/main/java/net/johnnyconsole/cp630/project/client/pane/PredictionPane.java
+++ temperature-client/src/main/java/net/johnnyconsole/cp630/project/client/pane/PredictionPane.java
@@ -3,10 +3,21 @@ package net.johnnyconsole.cp630.project.client.pane;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.control.Label;
+import net.johnnyconsole.cp630.project.client.util.Database;
+import weka.classifiers.trees.REPTree;
+import weka.core.Attribute;
+import weka.core.DenseInstance;
+import weka.core.Instances;
 
+import java.io.ObjectInputStream;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class PredictionPane extends GridPane {
 
@@ -16,6 +27,7 @@ public class PredictionPane extends GridPane {
         setHgap(10);
         setVgap(10);
 
+        TextField modelname = new TextField();
         ComboBox<Integer> years = new ComboBox<>(),
                 days = new ComboBox<>();
         ComboBox<String> months = new ComboBox<>();
@@ -52,11 +64,45 @@ public class PredictionPane extends GridPane {
             days.getSelectionModel().select(0);
         });
 
-        addRow(0, new Label("Year:"), years);
-        addRow(1, new Label("Month:"), months);
-        addRow(2, new Label("Day:"), days);
-        add(predict, 0, 3, 2, 1);
-        add(prediction, 0, 5, 2, 1);
+        predict.setOnAction(e -> {
+            try (Connection conn = Database.connect()){
+                if(modelname.getText() == null || modelname.getText().isEmpty()) prediction.setText("Model Missing");
+                else {
+                    String model = modelname.getText().toLowerCase();
+                    String sql = "SELECT object from cons3250_project_model WHERE name=?";
+                    PreparedStatement stmt = conn.prepareStatement(sql);
+                    stmt.setString(1, model);
+                    ResultSet set = stmt.executeQuery();
+                    if(set.next()) {
+                        REPTree tree = (REPTree) (new ObjectInputStream(set.getBinaryStream("object")).readObject());
+                        ArrayList<Attribute> attributes = new ArrayList<>(
+                                Arrays.asList(new Attribute("Year"),
+                                        new Attribute("Month"),
+                                        new Attribute("Day"),
+                                        new Attribute("Mean Temperature")));
+
+                        int year = years.getSelectionModel().getSelectedItem(),
+                                month = months.getSelectionModel().getSelectedIndex() + 1,
+                                day = days.getSelectionModel().getSelectedIndex() + 1;
+
+                        Instances p = new Instances("TemperaturePredict", attributes, 0);
+                        p.add(new DenseInstance(1, new double[]{year, month, day}));
+                        p.setClassIndex(p.numAttributes() - 1);
+                        double temp =  (int) (100 * tree.classifyInstance(p.firstInstance())) / 100.0;
+                        prediction.setText("The predicted temperature on " + day + getMonthName(month) + year + " is: " + temp + " °C");
+                    }
+                }
+            } catch(Exception ex) {
+                prediction.setText("Prediction Failed");
+            }
+        });
+
+        addRow(0, new Label("Model Name:"), modelname);
+        addRow(1, new Label("Year:"), years);
+        addRow(2, new Label("Month:"), months);
+        addRow(3, new Label("Day:"), days);
+        add(predict, 0, 4, 2, 1);
+        add(prediction, 0, 6, 10, 1);
 
         setMinWidth(1000);
     }
@@ -94,5 +140,22 @@ public class PredictionPane extends GridPane {
 
     private boolean isLeapYear(int year) {
         return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+    }
+
+    private String getMonthName(int month) {
+        switch(month) {
+            case 1: return " January ";
+            case 2: return " February ";
+            case 3: return " March ";
+            case 4: return " April ";
+            case 5: return " May ";
+            case 6: return " June ";
+            case 7: return " July ";
+            case 8: return " August ";
+            case 9: return " September ";
+            case 10: return " October ";
+            case 11: return " November ";
+            default: return " December ";
+        }
     }
 }
